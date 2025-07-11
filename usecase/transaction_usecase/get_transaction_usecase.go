cc19a3b78c1a41d07dccbae881f014951f38da61
susatyo441 <susatyo441@gmail.com>
2025-07-11 14:17:51 +0700

    fix: transaction list

--- usecase/transaction_usecase/get_transaction_usecase.go
+++ usecase/transaction_usecase/get_transaction_usecase.go
@@ -5,6 +5,7 @@ import (
 	transactiondto "be-shop-vision/dto/transaction"
 	transactionpipeline "be-shop-vision/pipeline/transaction"
 	"context"
+	"time"
 
 	utilDto "github.com/susatyo441/go-ta-utils/dto"
 	"github.com/susatyo441/go-ta-utils/entity"
@@ -21,7 +22,7 @@ func (u *TransactionUseCase) GetTransactionList(ctx context.Context, query dto.P
 	aggregateErr := u.TransactionService.Aggregate(
 		&result,
 		ctx,
-		transactionpipeline.GetTransactionPipeline(query, storeID),
+		transactionpipeline.GetTransactionPipeline(query, storeID, time.Now()),
 		options.Aggregate().SetCollation(&options.Collation{Strength: 3, Locale: "en"}),
 	)
 
